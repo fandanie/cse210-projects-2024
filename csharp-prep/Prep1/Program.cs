b3bca197be428723e3ea2f2c46cc67092b7eadb7
Daniel Fandino <daniel.fandino.baez@gmail.com>
2024-09-17 21:04:20 -0500

    my first proyect

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,21 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
+    { 
+
+		Console.Write("What is your first name?"); 
+
+		string name = Console.ReadLine(); 
+
+		 
+
+		Console.Write("What is your last name?"); 
+
+		string last = Console.ReadLine(); 
+
+		 
+
+		Console.WriteLine($"Your name is {name}, {name} {last}"); 
+
+	} 
 }
