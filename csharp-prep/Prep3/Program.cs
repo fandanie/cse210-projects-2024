bae833d707532b5ba0e8285df8e72b4a348bb807
Daniel Fandino <daniel.fandino.baez@gmail.com>
2024-09-19 07:59:13 -0500

    last update

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,38 @@
 using System;
-
-class Program
+					
+public class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+	public static void Main()
+	{
+        Console.WriteLine("Hello! We are going to play to guess the number, let's start.");
+
+        Random randomGenerator = new Random();
+        int num = randomGenerator.Next(1, 101);
+
+        int guess;
+
+        
+        do
+		{
+		Console.Write("What is your guess?"); 
+		string number2 = Console.ReadLine();
+		guess = int.Parse(number2);	
+
+		if (guess < num)
+		{
+			Console.WriteLine("Greater");
+		}
+			
+		else if (guess > num)
+		{
+			Console.WriteLine("Lower");
+		}
+		}	while (guess != num);
+
+        if (guess == num)
+        {
+            Console.WriteLine("You did it!");
+        }
+			
+	}
 }
