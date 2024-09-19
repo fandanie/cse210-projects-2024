6780756b05d83ceff193192e9b0c12aef818eaa8
Daniel Fandino <daniel.fandino.baez@gmail.com>
2024-09-18 20:24:13 -0500

    this is a new update

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,41 +1,47 @@
-using System;
+    using System;
 
-class Program
-{
-    static void Main(string[] args)
+    class Program
     {
-		Console.Write("What is your grade porcentage?");
-		string x = Console.ReadLine();
-		int grade = int.Parce(x);
-				
-		
-		if (grade >= 90)
-		{
-			Console.WriteLine("Your grade is A");
-			Console.WriteLine("Congratilations, you passed the course!");
-		}
-		else (grade >= 80)
-		{
-			Console.WriteLine("Your grade is B");
-			Console.WriteLine("Congratilations, you passed the course!");
-		}
-		else (grade >= 70)
-		{
-			Console.WriteLine("Your grade is C");
-			Console.WriteLine("Congratilations, you passed the course!");
-		}
-		else (grade >= 60)
-		{
-			Console.WriteLine("Your grade is D");
-		}
-		else if (grade > 60)
-		{
-			Console.WriteLine("Your grate than F");
-		}
-		else
-		{
-			Console.WriteLine("Please enter a valid grade");
-		}
-		
-	}
-}
+        static void Main(string[] args)
+        {
+            Console.Write("What is your grade porcentage?");
+            string x = Console.ReadLine();
+            int grade = int.Parse(x);
+
+            string letter = "";
+                    
+            
+            if (grade >= 90)
+            {
+                letter = "A";
+            }
+            else if (grade >= 80)
+            {
+                letter = "B";
+            }
+            else if (grade >= 70)
+            {
+                letter = "C";
+            }
+            else if(grade >= 60)
+            {
+                letter = "D";
+            }
+            else 
+            {
+                letter = "F";
+            }
+
+            Console.WriteLine($"Your grade is {letter}");
+
+            if (grade >= 70)
+            {
+                Console.WriteLine("You passed!");
+            }
+            else
+            {
+                Console.WriteLine("You did not passed, keep trying");
+            }
+            
+        }
+    }
