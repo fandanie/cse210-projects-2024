6096d8a35ef942707f8ae2db627de348040d9809
Daniel Fandino <daniel.fandino.baez@gmail.com>
2024-09-18 19:03:46 -0500

    new update

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+		Console.Write("What is your grade porcentage?");
+		string x = Console.ReadLine();
+		int grade = int.Parce(x);
+				
+		
+		if (grade >= 90)
+		{
+			Console.WriteLine("Your grade is A");
+			Console.WriteLine("Congratilations, you passed the course!");
+		}
+		else (grade >= 80)
+		{
+			Console.WriteLine("Your grade is B");
+			Console.WriteLine("Congratilations, you passed the course!");
+		}
+		else (grade >= 70)
+		{
+			Console.WriteLine("Your grade is C");
+			Console.WriteLine("Congratilations, you passed the course!");
+		}
+		else (grade >= 60)
+		{
+			Console.WriteLine("Your grade is D");
+		}
+		else if (grade > 60)
+		{
+			Console.WriteLine("Your grate than F");
+		}
+		else
+		{
+			Console.WriteLine("Please enter a valid grade");
+		}
+		
+	}
 }
