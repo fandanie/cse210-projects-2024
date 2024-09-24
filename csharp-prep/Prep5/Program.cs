60e09be30e3ec228dcb1a32329ac7bca20bd4105
Daniel Fandino <daniel.fandino.baez@gmail.com>
2024-09-23 21:10:18 -0500

    new update

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Globalization;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
+	{
+		DisplayWelcome();
+		string userName = PromptUserName();
+		int userNumber = FavNumber();
+        int SquaredNumber = SquareNumber(userNumber);
+        DisplayAll(userName, SquaredNumber);
+	}
+
+	static void DisplayWelcome()
+	{
+		Console.WriteLine("Welcome to the Program!");
+	}
+	
+	//--------User name--------
+	
+	static string PromptUserName()
+	{
+		Console.WriteLine("What is your name");
+		string userName = Console.ReadLine();
+        return userName;
+	}
+	//--------User Number----------
+	static int FavNumber()
+	{
+		Console.WriteLine("What is your favorite number?");
+		//string userNumber = Console.ReadLine();
+		int number = int.Parse(Console.ReadLine());
+
+        return number;
+	}
+    //-------Square----------
+
+    static int SquareNumber(int number)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        int square = number * number;
+
+        return square;
     }
+
+    //---------All------------
+
+    static void DisplayAll(string userName, int square)
+    {
+        Console.WriteLine($"Brother {userName} the square of your number is {square}");
+    }
+	
+	//Callings
+	
 }
